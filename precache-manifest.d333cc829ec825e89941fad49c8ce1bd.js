6f34fc0e3bcd97ace740a1a28219ed72967ad20f
Josh C <yichiehchan@gmail.com>
2022-01-26 12:35:45 +0800

    Updates

---
+++ precache-manifest.d333cc829ec825e89941fad49c8ce1bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9083570500a264ad84a7d1e93bdc73b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9010473264e78d32113a",
+    "url": "/static/css/main.84201dcc.chunk.css"
+  },
+  {
+    "revision": "d12eff549f9f086d4517",
+    "url": "/static/js/2.8fbc2a95.chunk.js"
+  },
+  {
+    "revision": "70380ff939c998672254",
+    "url": "/static/js/3.b7dfee96.chunk.js"
+  },
+  {
+    "revision": "9010473264e78d32113a",
+    "url": "/static/js/main.d1e30997.chunk.js"
+  },
+  {
+    "revision": "47a98b626ef86d6828ba",
+    "url": "/static/js/runtime~main.0413ad64.js"
+  },
+  {
+    "revision": "aa7156791d8d290bd81be9e625352cec",
+    "url": "/static/media/Logo.aa715679.png"
+  },
+  {
+    "revision": "354edcffda49b3de792888107ae73239",
+    "url": "/static/media/about.354edcff.png"
+  },
+  {
+    "revision": "c0606eb0df8aa4a7cb212f046a83903a",
+    "url": "/static/media/benefits.c0606eb0.png"
+  },
+  {
+    "revision": "21ba78575952cfcae988bbbcfff7fc3e",
+    "url": "/static/media/derivative-products.21ba7857.png"
+  },
+  {
+    "revision": "4cc317ecbb404f9f9eca9b7c01579d78",
+    "url": "/static/media/main-bg.4cc317ec.png"
+  },
+  {
+    "revision": "547d7ab15c864a6af6484920dba04650",
+    "url": "/static/media/ownership-frangmentation.547d7ab1.png"
+  },
+  {
+    "revision": "66eebf7364c02ebea56aff161827a0a2",
+    "url": "/static/media/parallel-stories.66eebf73.png"
+  },
+  {
+    "revision": "4ea9cd4ad5ad0f26f1e8ac6fa0043f0d",
+    "url": "/static/media/roadmap-bg.4ea9cd4a.png"
+  },
+  {
+    "revision": "f3177398c349706822800c32daababaf",
+    "url": "/static/media/social-media-bg.f3177398.png"
+  },
+  {
+    "revision": "4b4b3b8b6a64eb9858827912cb50917a",
+    "url": "/static/media/tree-structured-story.4b4b3b8b.png"
+  },
+  {
+    "revision": "4f80885c391bfc502ec37695d185ac64",
+    "url": "/static/media/write-more-earn-more.4f80885c.png"
+  }
+]);
