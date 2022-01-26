4100c9d2d2a69646d7d73e4623a139c458d83d46
Josh C <yichiehchan@gmail.com>
2022-01-26 17:49:49 +0800

    add image copyright

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,8 @@ function Footer() {
   return (
     <footer>
     {/* eslint-disable-next-line */}
-      <p>Made with <span role="img">❤️</span> by Starnest. ⓒ{year} All Right Reserved. </p>
+      <p>Made with <span role="img">❤️</span> by Starnest. ⓒ{year} All Right Reserved.
+      <br/>Photo by <a href="https://unsplash.com/@theshubhamdhage?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Shubham Dhage</a> on <a href="https://unsplash.com/@theshubhamdhage?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
     </footer>
   );
 }
